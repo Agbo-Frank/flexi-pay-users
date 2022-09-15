7ff9ef2d3abdfff296b544666f04ddc87ea023ca
Agbo Frank <agbofrank531@gmail.com>
2022-09-15 21:14:13 +0100

    Added meta data to product page

--- src/page/Products/Product.tsx
+++ src/page/Products/Product.tsx
@@ -45,6 +45,7 @@ export function Product(){
                 <meta property="og:image" content={product?.product_images[0].image_link} />
                 <meta property="og:image:url" content={product?.product_images[0].image_link} />
                 <meta property="og:image:secure_url" content={product?.product_images[0].image_link} />
+                <link rel="canonical" href="https://rankmath.com" />
                 <meta property="fb:add_id" content="769500967651765" />
                 <meta name="og:image" content={product?.product_images[0].image_link} />
                 <meta name="og:image:url" content={product?.product_images[0].image_link} />
