abf5aac7fc0cfd8e4fe6ee2b511d00f4282ba0c8
Agbo Frank <agbofrank531@gmail.com>
2022-10-01 06:14:11 +0100

    Added firebase

--- src/page/CountDown/service.tsx
+++ src/page/CountDown/service.tsx
@@ -34,7 +34,15 @@ export function FPFormikSubscribe(setLoading: Dispatch<SetStateAction<boolean>>)
             if(docs?.docs[0]?.data()){
                 docs?.docs?.forEach(async (d) => {
                     if(d.data().id === uuid){
-                        data = await updateDoc(doc(fireDB, "contact_info", d.id), {...docs.docs[0].data(),...value})
+                        if(value.email = ""){
+                            data = await updateDoc(doc(fireDB, "contact_info", d.id), {...docs.docs[0].data(), phone: value.phone})
+                        }
+                        else if(value.phone = ""){
+                            data = await updateDoc(doc(fireDB, "contact_info", d.id), {...docs.docs[0].data(), email: value.email})
+                        }
+                        else{
+                            data = await updateDoc(doc(fireDB, "contact_info", d.id), {...docs.docs[0].data(),...value})
+                        }
                         console.log(data)
                     }
                 })
