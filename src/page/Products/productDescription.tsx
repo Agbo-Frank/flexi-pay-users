c2bdbb40b76bfa4491d8a776e568b33b0edcb9f3
Agbo Frank <agbofrank531@gmail.com>
2022-10-04 09:00:30 +0100

    Added the product description

--- src/page/Products/productDescription.tsx
+++ src/page/Products/productDescription.tsx
@@ -1,41 +1,43 @@
 import { IProduct } from "../../interface"
-import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html'
 import { memo } from "react";
 import React from 'react' ;
 import { Parser, ProcessNodeDefinitions } from 'html-to-react' ;
 
 function DataDisplay({header, body}: {header: string, body?: string}){
-    const deltaOps =  [
-        {insert: body}
+    const processNodeDefinitions = new ProcessNodeDefinitions(React);
+    const processingInstructions = [
+        {
+            replaceChildren: true,
+            shouldProcessNode: function (node: any) {
+                return node.parent && node.parent.name && node.parent.name === 'h1';
+            },
+            processNode: function (node: any, children: any) {
+                return node.data.toUpperCase();
+            }
+        },
+        {
+          // Anything else
+          shouldProcessNode: function (node: any) {
+            return true;
+          },
+          processNode: processNodeDefinitions.processDefaultNode
+        }
     ];
     
-    const cfg = {};
-    const converter = new QuillDeltaToHtmlConverter(deltaOps, cfg);
+    const htmlToReactParser = new Parser();
+    const html = htmlToReactParser.parseWithInstructions(body, () => true, processingInstructions);
+    // const reactHtml = renderToStaticMarkup(reactComponent);
+    // console.log(html)
 
-    const html = converter.convert(); 
-    const processNodeDefinitions = new ProcessNodeDefinitions(React);
-    console.log(html)
-
-    return(
+    return (
         <div className="mx-2">
             <div className="border py-2 px-3 font-medium text-lg capitalize">{header}</div>
-            <div className="border border-t-0 py-2 px-3">{html || "Loading..."}</div>
+            <div className="border border-t-0 py-2 px-3 whitespace-pre-wrap">{html|| "Loading..."}</div>
         </div>
     )
 }
 
 export function ProductDescription({product}: {product: IProduct | undefined}){
-    const deltaOps =  [
-        {insert: product?.description}
-    ];
-    
-    const cfg = {
-        encodeHtml: false
-    };
-    const converter = new QuillDeltaToHtmlConverter(deltaOps, cfg);
-
-    const html = converter.convert(); 
-    console.log(html)
     return(
         <div className="space-y-4">
             <DataDisplay 
