58d2fd0dddc78050a937850aa273158e458d3178
Agbo Frank <agbofrank531@gmail.com>
2022-11-04 23:03:48 +0100

    added react snap

--- src/index.tsx
+++ src/index.tsx
@@ -12,24 +12,29 @@ import { CookiesProvider } from 'react-cookie';
 import { HelmetProvider } from 'react-helmet-async';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration.';
 
-ReactDOM.hydrate(
-  (
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <ThemeProvider theme={theme}>
-            <CookiesProvider>
-              <HelmetProvider>
-                < App />
-              </HelmetProvider>
-            </CookiesProvider>
-          </ThemeProvider>
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>
-  ),
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById("root");
+
+const Application = () => (
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CookiesProvider>
+            <HelmetProvider>
+              < App />
+            </HelmetProvider>
+          </CookiesProvider>
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
+
+if (rootElement?.hasChildNodes()) {
+  ReactDOM.hydrate((<Application />),rootElement as HTMLElement);
+} else {
+  ReactDOM.render((<Application />),rootElement as HTMLElement);
+}
 
 serviceWorkerRegistration.register();
 
